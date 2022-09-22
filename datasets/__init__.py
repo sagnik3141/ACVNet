70ad6f6eac5dccea588f365df8fcacda708d78be
GopiRajuMatta <gopirajurgukt123@gmail.com>
2022-09-22 19:46:33 +0530

    Added SPC Dataset

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,7 +1,9 @@
 from .kitti_dataset_1215 import KITTIDataset
 from .sceneflow_dataset import SceneFlowDatset
+from .kitti_dataset_multi import KITTIDatasetSPC
 
 __datasets__ = {
     "sceneflow": SceneFlowDatset,
-    "kitti": KITTIDataset
+    "kitti": KITTIDataset,
+    "kitti_spc_multi": KITTIDatasetSPC
 }
