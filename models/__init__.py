2ff6ec32bb00d66003ed1b60b24b36beff602223
Sagnik Ghosh <ee19b132@smail.iitm.ac.in>
2022-09-22 19:28:08 +0530

    Added spc loss

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 from models.acv import ACVNet
 from models.acv_spc_multi import ACVSPC
-from models.loss import model_loss_train_attn_only, model_loss_train_freeze_attn, model_loss_train, model_loss_test
+from models.loss import model_loss_train_attn_only, model_loss_train_freeze_attn, model_loss_train, model_loss_test, model_loss_train_spc
 
 __models__ = {
     "acvnet": ACVNet,
