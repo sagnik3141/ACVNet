622a990f845edc41ff53257ac3992dcaff4b3197
Sagnik Ghosh <ee19b132@smail.iitm.ac.in>
2022-09-23 23:21:04 +0530

    Rename dataset class

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,9 +1,11 @@
 from .kitti_dataset_1215 import KITTIDataset
 from .sceneflow_dataset import SceneFlowDatset
 from .kitti_dataset_multi import KITTIDatasetSPC
+from .kitti_dataset_spc import KITTIDatasetSPCSingle
 
 __datasets__ = {
     "sceneflow": SceneFlowDatset,
     "kitti": KITTIDataset,
-    "kitti_spc_multi": KITTIDatasetSPC
+    "kitti_spc_multi": KITTIDatasetSPC,
+    "kitti_spc": KITTIDatasetSPCSingle
 }
